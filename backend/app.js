590888d92c1969ca18aa3b5417fa5b1576ce8b41
mohd shahan siddiqui <133672067+blaackstring@users.noreply.github.com>
2025-05-07 18:05:35 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['*'],
   methods: ['POST', 'PUT', 'GET', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Authorization', 'Content-Type'],
